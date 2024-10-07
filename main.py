c5a003b02c90cf2016e25f452a7bee9771a41e5d
Arpit Agarwal <arpitagarwal1308@gmail.com>
2024-10-07 12:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import hello_world, assignments
+
 
 app = FastAPI()
 
-# Include the hello_world router
-app.include_router(hello_world.router)
-
-# Include the assignments' router
-app.include_router(assignments.router)
 
 # Root endpoint example
 @app.get("/")
