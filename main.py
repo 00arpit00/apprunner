9873f317ced3577d1d531a5209075fb82293d125
Arpit Agarwal <arpitagarwal1308@gmail.com>
2024-10-07 14:52:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-
+import uvicorn
 
 app = FastAPI()
 
-
+print("Starting webserver...")
+uvicorn.run(
+    app,
+    host="0.0.0.0",
+    port=8000,
+    debug=os.getenv("DEBUG", True),
+    log_level=os.getenv('LOG_LEVEL', "debug")
+)
 # Root endpoint example
 @app.get("/")
 def read_root():
